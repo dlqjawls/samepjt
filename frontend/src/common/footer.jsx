b7b9d7cc1d578d9c3af2ee01b36e097863d39f14
박수연 <magnoliasx03@gmail.com>
2025-01-21 15:02:29 +0900

    common/futter footer.jsx 초안 생성 Footer.css 초안 생성

--- frontend/src/common/footer.jsx
+++ frontend/src/common/footer.jsx
@@ -1,11 +1,24 @@
+import "./Footer.css";
 
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <div className="footer-content">
+        <img src="/path/to/car_icon.jpg" alt="Car Icon" className="car-icon" />
+        <div className="footer-links">
+          <a href="#privacy-policy">개인정보 처리방침</a>
+          <span>|</span>
+          <a href="#terms">이용 약관</a>
+          <span>|</span>
+          <a href="#announcements">공지사항</a>
+        </div>
+        <div className="customer-center">고객센터: 000-000-0000</div>
+        <div className="copyright">
+          COPYRIGHT © MODUCAR. ALL RIGHTS RESERVED.
+        </div>
+      </div>
+    </footer>
+  );
+};
 
-const footer = () => {
-    return (
-    <div>
-        푸터
-    </div>
-)
-}
-
-export default footer;
+export default Footer;
