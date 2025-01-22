beb257a18583c7155337e89fac081ba180ed6c4d
박수연 <magnoliasx03@gmail.com>
2025-01-22 17:28:51 +0900

    feat: frontend-footer 코드 수정

--- frontend/src/common/footer.jsx
+++ frontend/src/common/footer.jsx
@@ -4,17 +4,26 @@ const Footer = () => {
   return (
     <footer className="footer">
       <div className="footer-content">
-        <img src="/path/to/car_icon.jpg" alt="Car Icon" className="car-icon" />
-        <div className="footer-links">
-          <a href="#privacy-policy">개인정보 처리방침</a>
-          <span>|</span>
-          <a href="#terms">이용 약관</a>
-          <span>|</span>
-          <a href="#announcements">공지사항</a>
+        <div className="footer-left-content">
+          <img
+            src="../public/car_icon.jpg" //TODO: local에서 작업했기 때문에 경로 재설정 해줘야함
+            alt="Car Icon"
+            className="car-icon"
+          />
         </div>
-        <div className="customer-center">고객센터: 000-000-0000</div>
-        <div className="copyright">
-          COPYRIGHT © MODUCAR. ALL RIGHTS RESERVED.
+
+        <div className="footer-right-content">
+          <div className="footer-links">
+            <a href="#privacy-policy">개인정보 처리방침</a>
+            <span>|</span>
+            <a id = "terms" href="#terms">이용 약관</a>
+            <span>|</span>
+            <a id = "announcements" href="#announcements">공지사항</a>
+          </div>
+          <div className="customer-center">고객센터: 000-000-0000</div>
+          <div className="copyright">
+            COPYRIGHT © MODUCAR. ALL RIGHTS RESERVED.
+          </div>
         </div>
       </div>
     </footer>
