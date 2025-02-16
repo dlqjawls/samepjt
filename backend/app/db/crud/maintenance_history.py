0d4f134c170f971683b37958f60307c4ebb9ab27
송명석 <sms4621875@naver.com>
2025-02-16 09:45:29 +0900

    fix: maintenance history 수정

--- backend/app/db/crud/maintenance_history.py
+++ backend/app/db/crud/maintenance_history.py
@@ -1,13 +1,23 @@
 from app.db.models.maintenance_history import MaintenanceHistory
 from app.db.crud.base import CRUDBase
 from sqlmodel import Session, select
-from typing import List
+from typing import List, Optional, Union, cast
+from app.db.models.vehicle import Vehicle
+from app.db.models.module import Module
+from app.db.models.option import Option
+from app.utils.lut_constants import ItemType
+from app.utils.exceptions import ConflictError
 
 class MaintenanceHistoryCRUD(CRUDBase[MaintenanceHistory]):
     def __init__(self):
         super().__init__(MaintenanceHistory)
         
+    def get_by_id(self, session: Session, maintenance_id: int) -> Optional[MaintenanceHistory]:
+        query = select(self.model).where(self.model.maintenance_id == maintenance_id)
+        return session.exec(query).first()
+        
     def get_item_maintenance_history(self, session: Session, item_id: int, item_type_id: int) -> List[MaintenanceHistory]:
+        """주어진 item_id, item_type_id에 해당하는 정비 기록 목록을 조회합니다."""
         query = (
             select(self.model)
             .where(
@@ -28,6 +38,14 @@ class MaintenanceHistoryCRUD(CRUDBase[MaintenanceHistory]):
         )
         result = session.exec(query).first()
         return result is not None
-
+  
+    def fetch_item(self, session: Session, item_type_id: int, item_id: int) -> Optional[Union[Vehicle, Module, Option]]:
+        """주어진 item_type_id, item_id에 해당하는 아이템을 조회합니다."""
+        mapping = {"vehicle": Vehicle, "module": Module, "option": Option}
+        model = mapping.get(ItemType.get_name(item_type_id))
+        if model is None:
+            raise ConflictError(f"Invalid item type: {ItemType.get_name(item_type_id)  }")
+        return cast(Optional[Union[Vehicle, Module, Option]], session.get(model, item_id))
+      
 
 maintenance_history_crud = MaintenanceHistoryCRUD()
